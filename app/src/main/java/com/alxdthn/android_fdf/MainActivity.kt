8f5c8f402e69c85aeb9939be66eaf9e2289b5883
alxdthn <alxdthn@gmail.com>
2019-12-15 21:31:56 +0300

    add readme

--- app/src/main/java/com/alxdthn/android_fdf/MainActivity.kt
+++ app/src/main/java/com/alxdthn/android_fdf/MainActivity.kt
@@ -2,16 +2,11 @@ package com.alxdthn.android_fdf
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
 
-		btn.setOnClickListener {
-			fdfView.file = "mars.fdf"
-		}
 	}
 }
